3d022ff57f9b58f80b10e136f0d22c0808b9fa62
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-01-06 02:52:21 +0100

    Add music downloader

---
+++ download_music.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir music
+cd music
+youtube-dl -x https://www.youtube.com/watch?v=JaflMqvxIqA
+youtube-dl -x https://www.youtube.com/watch?v=dcSPK5yn9K8
+youtube-dl -x https://www.youtube.com/watch?v=wRWq53IFXVQ
+youtube-dl -x https://www.youtube.com/watch?v=ZTg6hg1miFg
+youtube-dl -x https://www.youtube.com/watch?v=IljKQ1BdHKI
+youtube-dl -x https://www.youtube.com/watch?v=mt9qJqj3wQw
+youtube-dl -x https://www.youtube.com/watch?v=RaK9OSSOqQM
+youtube-dl -x https://www.youtube.com/watch?v=BnZu9uOH-nY
+youtube-dl -x https://www.youtube.com/watch?v=Q3I1fx1GOuc
+youtube-dl -x https://www.youtube.com/watch?v=-uInmFU6JkU
+youtube-dl -x https://www.youtube.com/watch?v=s0krxH0dWhU
+youtube-dl -x https://www.youtube.com/watch?v=3q7oJuyy5Ac
+youtube-dl -x https://www.youtube.com/watch?v=E9s1ltPGQOo
+youtube-dl -x https://www.youtube.com/watch?v=Hy0aEj85ifY
+youtube-dl -x https://www.youtube.com/watch?v=hUiAmrtxits
+youtube-dl -x https://www.youtube.com/watch?v=le4N3TI1Uz0
+youtube-dl -x https://www.youtube.com/watch?v=kx8af6vE3Rc
+youtube-dl -x https://www.youtube.com/watch?v=43IPAGw01IY
+youtube-dl -x https://www.youtube.com/watch?v=4Ruqn1Lnexs
+youtube-dl -x https://www.youtube.com/watch?v=Bx6nOkdeNKs
+youtube-dl -x https://www.youtube.com/watch?v=o78T9-I4OGA
+youtube-dl -x https://www.youtube.com/watch?v=8avMLHvLwRQ
+youtube-dl -x https://www.youtube.com/watch?v=go_XgaJNVgg
+youtube-dl -x https://www.youtube.com/watch?v=AUyqvUG0ypA
